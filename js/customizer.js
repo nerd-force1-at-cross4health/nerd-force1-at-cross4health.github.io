03b90dc2ed61656e96e2054bb0b27a7b3965b4cc
drvannacut <45701464+drvannacut@users.noreply.github.com>
2018-12-08 02:07:15 +0100

    minor correction of jquery stuff

--- js/customizer.js
+++ js/customizer.js
@@ -2,12 +2,18 @@
 EKG Javascript
 */
 
-var currentHeadline = 0;
+var offsetTopChart = 0;
 
 $(document).ready(function(){
 
-    if ($(".chart").length) {
+    offsetTopChart = $(".chart-container").offset().top;
 
+});
+
+$(window).scroll(function(){
+
+    if ($(".chart").length && ( $(window).scrollTop() + ($(window).height() / 2) >= offsetTopChart )) {
+        console.log("start calc " + offsetTopChart)
         $(".chart").each(function(){
 
             $(this).find("span.movingChart").css("width", $(this).find(".chart-bar").attr("data-chart-fill") + "%");
@@ -15,5 +21,6 @@ $(document).ready(function(){
         });
 
     }
+
 
 });
