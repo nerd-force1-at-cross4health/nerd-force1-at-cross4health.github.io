268ba68b1a5587a2cd941dea0877e82c6f78e581
drvannacut <45701464+drvannacut@users.noreply.github.com>
2018-12-08 02:12:55 +0100

    final correction of chart calc

--- js/customizer.js
+++ js/customizer.js
@@ -6,21 +6,25 @@ var offsetTopChart = 0;
 
 $(document).ready(function(){
 
-    offsetTopChart = $(".chart-container").offset().top;
+    offsetTopChart = $(".chart-container").offset().top - ($(window).height() / 1.3);
 
 });
 
 $(window).scroll(function(){
 
-    if ($(".chart").length && ( $(window).scrollTop() + ($(window).height() / 2) >= offsetTopChart )) {
-        console.log("start calc " + offsetTopChart)
-        $(".chart").each(function(){
+    setTimeout(function(){
+        if ($(".chart").length && ( $(window).scrollTop() >= offsetTopChart )) {
+            console.log("start calc " + offsetTopChart)
+            $(".chart").each(function(){
+    
+                $(this).find("span.movingChart").css("width", $(this).find(".chart-bar").attr("data-chart-fill") + "%");
+    
+            });
+    
+        }
+      }, 1000);
 
-            $(this).find("span.movingChart").css("width", $(this).find(".chart-bar").attr("data-chart-fill") + "%");
-
-        });
-
-    }
+  
 
 
 });
