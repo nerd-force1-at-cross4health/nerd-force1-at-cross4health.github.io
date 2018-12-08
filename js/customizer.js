c12db0ebe5faaff03caae4a30fbfe0b92b9e4867
drvannacut <45701464+drvannacut@users.noreply.github.com>
2018-12-08 01:59:45 +0100

    fancy animation for diseases

--- js/customizer.js
+++ js/customizer.js
@@ -6,4 +6,14 @@ var currentHeadline = 0;
 
 $(document).ready(function(){
 
+    if ($(".chart").length) {
+
+        $(".chart").each(function(){
+
+            $(this).find("span.movingChart").css("width", $(this).find(".chart-bar").attr("data-chart-fill") + "%");
+
+        });
+
+    }
+
 });
